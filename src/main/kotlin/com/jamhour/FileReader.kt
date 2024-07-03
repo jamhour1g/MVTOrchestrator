1e71144bdc42c0a33869d89c405ee5b00b9620b2
jamhour1g <yazanjam50@gmail.com>
2024-07-03 20:42:17 +0300

    feat(FileReader): Introduce readFile(Path) method returning a list of Process objects after parsing and processing files

---
+++ src/main/kotlin/com/jamhour/FileReader.kt
@@ -0,0 +1,42 @@
+package com.jamhour
+
+import kotlinx.coroutines.coroutineScope
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.logging.Logger
+import java.util.regex.Pattern
+
+
+/**
+ * Regular expression pattern to match process information in the input file.
+ * The pattern expects three space-separated integer values for process ID, size, and time.
+ */
+val linePatten: Pattern =
+    Pattern.compile("(?<processId>\\d+)\\s+(?<processSize>\\d+)\\s+(?<processTime>\\d+)")
+
+/**
+ * Logger instance for the FileReader.
+ */
+private val logger = Logger.getLogger("FileReader")
+
+/**
+ * Reads a file containing process information and returns a list of Process objects.
+ *
+ * @param filePath The path to the file to be read.
+ * @return A list of Process objects parsed from the file. Returns an empty list if the file cannot be read or parsed.
+ */
+suspend fun readFile(filePath: Path): List<Process> = coroutineScope {
+    runCatching {
+        Files.newBufferedReader(filePath).useLines { seq ->
+            return@runCatching seq.map { linePatten.matcher(it) }
+                .filter { it.matches() }
+                .map {
+                    val processId = it.group("processId")!!.toInt()
+                    val processSize = it.group("processSize")!!.toInt()
+                    val processTime = it.group("processTime")!!.toInt()
+                    Process(processId, processSize, processTime).also { logger.fine { "Created process: $it" } }
+                }
+                .toList().also { logger.fine { "Created list of processes with size ${it.size}" } }
+        }
+    }.getOrElse { emptyList<Process>().also { logger.severe { "Failed to read file: $it with error: $it returning an empty list" } } }
+}
