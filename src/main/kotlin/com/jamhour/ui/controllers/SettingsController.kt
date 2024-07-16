2fb480d33f8ecb6d0d4366a4682db0c03bfa9860
jamhour1g <yazanjam50@gmail.com>
2024-07-16 09:44:53 +0300

    refactor(SettingsController): Remove logsTile related logic

--- src/main/kotlin/com/jamhour/ui/controllers/SettingsController.kt
+++ src/main/kotlin/com/jamhour/ui/controllers/SettingsController.kt
@@ -1,7 +1,6 @@
 package com.jamhour.ui.controllers
 
 import atlantafx.base.controls.Tile
-import atlantafx.base.controls.ToggleSwitch
 import com.jamhour.util.Defaults.defaultTheme
 import com.jamhour.util.SettingsParam
 import com.jamhour.util.Theme
@@ -15,9 +14,6 @@ class SettingsController {
     @FXML
     private lateinit var themeTile: Tile
 
-    @FXML
-    private lateinit var logsTile: Tile
-
     private val themesComboBox = ComboBox<Theme>().apply {
         items += Theme.entries
         selectionModel.select(defaultTheme)
@@ -28,26 +24,16 @@ class SettingsController {
         }
     }
 
-    private val logsSwitch = ToggleSwitch("Enabled").apply {
-        setSelected(true)
-        selectedProperty().addListener { _, _, newValue ->
-            text = if (newValue && isSelected) "Enabled" else "Disabled"
-        }
-    }
-
     @FXML
     fun initialize() {
         themeTile.action = themesComboBox
-        logsTile.action = logsSwitch
-
         loadSettings(loadStoredParams().settings)
     }
 
     private fun loadSettings(settings: SettingsParam) {
         themesComboBox.selectionModel.select(settings.theme)
-        logsSwitch.isSelected = settings.logs
     }
 
-    fun saveSettings() = SettingsParam(themesComboBox.value, logsSwitch.isSelected)
+    fun saveSettings() = SettingsParam(themesComboBox.value)
 
 }
